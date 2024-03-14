b4344c2929719ab78a68e10ad55d1626e789f4de
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-03-14 20:01:47 +0000

    img remove and text update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import Hero from "../components/Hero";
-import Bio from "../components/Bio";
 import Projects from "../components/Projects";
-import BlogCards from "../components/BlogCards";
+
 
 const Home = () => {
   return (
