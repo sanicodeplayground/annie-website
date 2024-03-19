6b3681a063584cb7a970d92c0efa371c62771ccf
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-03-19 19:55:18 +0000

    Update text on About Me page

--- src/components/BioExtended.jsx
+++ src/components/BioExtended.jsx
@@ -1,21 +1,42 @@
 function BioExtended() {
   return (
     <section className="text-custom-grey">
-      <article className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md  leading-loose [&>p]:mt-3">
-        <h2 className="mb-4 text-4xl  font-extrabold">
-          About me
-        </h2>
+      <article className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md  leading-loose [&>p]:mt-3 [&>ul]:mt-3">
+        <h2 className="mb-4 text-4xl  font-extrabold">About me</h2>
 
-        <h3 className="mb-4 mt-8 text-3xl  font-extrabold">
+        {/* <h3 className="mb-4 mt-8 text-3xl  font-extrabold">
           As an illustrator
-        </h3>
+        </h3> */}
         <p>
-          I am Annie, a freelance graphic designer & illustrator based in Winnipeg, Canada. I’ve had over seven years of agency experience, and have placed my main focus on Illustration work, Branding and Identity projects.
+          Hello! My name is Annie, and I am an illustrator currently based in
+          Bristol. You can usually find me scribbling away outdoors in
+          frequented spots. I also love getting involved with our local flora
+          and fauna directly, through both my work as an ornithologist and my
+          time spent ringing birds.
         </p>
         <p>
-          Growing up, I watched an endless amount of animations and was always in awe of beautifully illustrated books, which ended up paving my path to becoming a designer and illustrator. When I'm away from my laptop, I am busy being a cat mom, a video gaming nerd and trying to get outside as much as possible, to enjoy the outdoors.
+          Following the receipt of my BA degree in Illustration from The
+          University of West England, I have been working on getting the
+          narratives of both prevailing and endangered species into the world. I
+          do so through my art, creating stories and worlds through my
+          illustrations which I hope will inspire my readers to learn more about
+          the world around them.
         </p>
-       
+        <p>
+          This website is a showcase of my work, both previous and current,
+          including my illustrations and children&apos;s book designs. My focus
+          is largely around natural history illustration, from both writing and
+          designing fictional and non-fictional works. I love working with any
+          analogue medium but I&apos; m particularly drawn to watercolour,
+          graphite and ink work.
+        </p>
+        <ul>
+          <li>Winner of the Batsford Prize people&apos;s choice award 2022</li>
+          <li>Highly commended by Pan Macmillan Prize 2022</li>
+          <li>
+            Runner up in the Batsford Prize 2022 Children&apos;s Illustration.
+          </li>
+        </ul>
       </article>
     </section>
   );
