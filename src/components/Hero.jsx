b4a613fceeb07e23c158c15d4fdc53008e9602eb
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-03-22 13:20:38 +0000

    update hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 function Hero() {
   return (
 
-    <section className="bg-[url('/images/hero4.jpg')] w-full bg-left bg-cover relative">
-    <div className="absolute inset-0 bg-gradient-to-l from-black to-transparent z-10"></div>
-    <div className="absolute inset-0 bg-black bg-opacity-20 z-10"></div>
+    <section className="bg-[url('/images/annie1.jpg')] w-full bg-right bg-cover relative">
+    {/* <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-10"></div> */}
+    <div className="absolute inset-0 bg-black bg-opacity-10 z-10"></div>
 
     <div className="flex justify-end mx-auto max-w-screen-xl py-20 sm:py-40 px-4 relative z-20">
-        <div className="max-w-screen-md">
-          <h2 className="subhead mb-4 text-4xl md:text-5xl lg:text-6xl text-custom-light">
+        <div className="max-w-md">
+          <h2 className="mb-4 text-4xl md:text-5xl lg:text-6xl text-custom-light">
             Annie is an illustrator based in Bristol
           </h2>
           <p className="mb-8 text-lg max-w-md">
