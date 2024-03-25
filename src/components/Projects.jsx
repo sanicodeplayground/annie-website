c27595228ea8ec7e7c4e3d1496ffc6a38b40a58a
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-03-25 15:29:30 +0000

    add single portfolio item

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,7 @@
-import { Link } from 'react-router-dom';
-import Hero from "../pages/SingleProjectTales.jsx";
+import { Link } from "react-router-dom";
+import SingleProjectTales from "../pages/SingleProjectTales";
 
 function Projects() {
-  
   // Array of projects
   const projects = [
     {
@@ -33,7 +32,7 @@ function Projects() {
       alt: "Finding The Wild",
       description: "Finding The Wild",
     },
-  ]
+  ];
   return (
     <section>
       <h2 className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mb-4 text-4xl font-extrabold text-custom-primary">
@@ -43,14 +42,10 @@ function Projects() {
       {/* projects grid */}
       <ul className="grid sm:max-w-[52.5rem] px-4 grid-cols-1 sm:grid-cols-2  mx-auto gap-6 lg:gap-y-8 xl:gap-x-8 lg:px-8 lg:max-w-6xl pb-20">
         {projects.map((project, index) => (
-          <li key={index} >
+          <li key={index}>
             <Link to={project.to} title={project.title}>
-              <img
-              src={project.src}
-              alt={project.alt}
-              className="rounded-xl"
-              />
-              <h3 className="leading-6 text-custom-primary font-semibold md:text-xl mb-4 mt-6" >
+              <img src={project.src} alt={project.alt} className="rounded-xl" />
+              <h3 className="leading-6 text-custom-primary font-semibold md:text-xl mb-4 mt-6">
                 {project.description}
               </h3>
             </Link>
