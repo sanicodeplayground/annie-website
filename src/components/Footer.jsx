2787c01ff58a61ff5c54eba03bb3dd8f130ffa54
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-03-26 07:35:23 +0000

    change rounded corners in portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-
-    <footer className="text-center text-sm pt-20 pb-10 sm:p-6 text-custom-primary">
-      <p>All artwork © Annie Brooker / <a className="md:w-1/4 underline" href="https://www.instagram.com/anniebookerart/" title="Annie's Instagram" target="_blank">📸 @anniebookerart</a></p>
+    <footer className="text-center text-sm pt-20 pb-10 sm:p-6">
+      <p>
+        All artwork © Annie Brooker /{" "}
+        <a
+          className="md:w-1/4 underline hover:text-custom-hover"
+          href="https://www.instagram.com/anniebookerart/"
+          title="Annie's Instagram"
+          target="_blank"
+        >
+          📸 @anniebookerart
+        </a>
+      </p>
     </footer>
-
   );
 }
 
